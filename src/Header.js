c3db25041a30c46a9d62ed87084c6ad688ccdf5e
Vrouqen <108952975+Vrouqen@users.noreply.github.com>
2025-05-19 23:34:09 -0500

    Modificaciones en header

--- src/Header.js
+++ src/Header.js
@@ -8,7 +8,8 @@ function Header() {
         <ul>
           <li><a href="http://3.218.233.15:777/">Inicio</a></li>
           <li><a href="https://herokud1-03800266aff2.herokuapp.com/">Lácteos</a></li>
-          <li><a href="http://146.190.160.148:999/">Carnes y Bebidas</a></li>
+          <li><a href="http://146.190.160.148:999/">Carnes</a></li>
+          <li><a href="https://versel-cyan.vercel.app/">Bebidas</a></li>
           <li><a href="https://ejercicio2-railway.up.railway.app/">Limpieza</a></li>
         </ul>
       </nav>
